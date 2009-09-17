095131a9544a103851ea152cbdee04fc5fbd3dde
Vinod Kurup <vinod@kurup.com>
2009-09-17 14:39:04 -0400

    Changed format of delicious import
    Previous version required a delicious password (phpdelicious).
    
    This format uses the javascript widget directly from delicious.com.
    
    Edited the stylesheet to format the results so it was similar to previous version.

---
+++ news.php
@@ -0,0 +1,7 @@
+<?php include("header.php"); ?>
+
+<script type="text/javascript"
+        src="http://feeds.delicious.com/v2/js/lawbeattie?title=News&icon=n&count=15&sort=date&extended&name&showadd"></script>
+
+
+<?php include("footer.php"); ?>
