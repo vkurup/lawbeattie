4d4c8bb060928c692b943004b8ccbf2ca6498ce5
Vinod Kurup <vinod@kurup.com>
2013-08-10 18:12:22 -0400

    New address

--- contact.php
+++ contact.php
@@ -3,11 +3,12 @@
 <h3 class="top_main_heading">Contact Us</h3>
 
 <p>
-Bull City Business Center<br />
-115 Market Street, Suite 204G<br />
-Durham, NC 27701<br />
+P.O. Box 1298<br />
+100 N. Churton St, Suite 208<br />
+Hillsborough, NC 27278<br />
 <br />
 Tel: (919) 688-5171<br />
+Fax: (919) 287-2290<br />
 Email: lawbeattie@gmail.com<br />
 Facebook: <a href="http://www.facebook.com/pages/Durham-NC/Law-Office-of-Ethan-O-Beattie/76305578018">View our facebook page.</a>
 </p>
