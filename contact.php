17124c0a099a0b3bec022f80b88a1f9c7a547262
Vinod Kurup <vinod@kurup.com>
2013-08-10 18:31:24 -0400

    Add Google map

--- contact.php
+++ contact.php
@@ -4,7 +4,7 @@
 
 <p>
 P.O. Box 1298<br />
-100 N. Churton St, Suite 208<br />
+<a href="https://maps.google.com/maps?q=100+N+Churton+St,+Hillsborough,+NC&hl=en&sll=35.170517,-79.860994&sspn=7.666303,16.907959&oq=100+N.+Chu&hnear=100+N+Churton+St,+Hillsborough,+North+Carolina+27278&t=m&z=17&iwloc=A">100 N. Churton St, Suite 208</a><br />
 Hillsborough, NC 27278<br />
 <br />
 Tel: (919) 688-5171<br />
@@ -12,5 +12,7 @@ Fax: (919) 287-2290<br />
 Email: lawbeattie@gmail.com<br />
 Facebook: <a href="http://www.facebook.com/pages/Durham-NC/Law-Office-of-Ethan-O-Beattie/76305578018">View our facebook page.</a>
 </p>
+
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=100+N+Churton+St,+Hillsborough,+NC&amp;aq=0&amp;oq=100+N.+Chu&amp;sll=35.170517,-79.860994&amp;sspn=7.666303,16.907959&amp;ie=UTF8&amp;hq=&amp;hnear=100+N+Churton+St,+Hillsborough,+North+Carolina+27278&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=36.075313,-79.099531&amp;output=embed"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=100+N+Churton+St,+Hillsborough,+NC&amp;aq=0&amp;oq=100+N.+Chu&amp;sll=35.170517,-79.860994&amp;sspn=7.666303,16.907959&amp;ie=UTF8&amp;hq=&amp;hnear=100+N+Churton+St,+Hillsborough,+North+Carolina+27278&amp;t=m&amp;z=14&amp;iwloc=A&amp;ll=36.075313,-79.099531" style="color:#0000FF;text-align:left">View Larger Map</a></small>
 
 <?php include("footer.php"); ?>
