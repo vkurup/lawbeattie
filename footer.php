204dcef6c4ceb3e56cd6a497909e6f00cc5c2aa1
Vinod Kurup <vinod@kurup.com>
2017-08-03 23:32:40 -0400

    Update copyright

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div id="footer"><div class="spacer"></div>
 <hr />
 <p class="left"></p>
-<p class="right">&copy;2008-2016 Beattie Law Firm, PLLC</p>
+<p class="right">&copy;2008-2017 Beattie Law Firm, PLLC</p>
 <p>&nbsp;</p>
 </div> <!-- end footer -->
 
